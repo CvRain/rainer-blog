24b7f3625c7c021a29a9c49c10174ace27f9fcbc
cvrain <cvraindays@outlook.com>
2025-07-27 23:15:35 +0800

    add a test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestPageComponent } from './test-page/test-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
