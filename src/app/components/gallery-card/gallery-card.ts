ff49fcc7bab61bcb515882c03d6762478d5c324b
cvrain <cvraindays@outlook.com>
2025-07-30 21:47:17 +0800

    The introSelfCard component has been improved and the placement of friend links has been modified

---
+++ src/app/components/gallery-card/gallery-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-card',
+  imports: [],
+  templateUrl: './gallery-card.html',
+  styleUrl: './gallery-card.css'
+})
+export class GalleryCard {
+
+}
