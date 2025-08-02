5c3260338380aef66769cf48d6b2a701da32b045
cvrain <cvraindays@outlook.com>
2025-08-02 12:08:03 +0800

    add environment setting file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -9,4 +10,6 @@ import { CommonModule } from '@angular/common';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+  copyright = environment.copyright;
+  icp = environment.icp;
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:4000/api",
+  copyright: "Copyright © 2020-2021 cvrain",
+  version: "1.0.0",
+  icp: ""
+};
