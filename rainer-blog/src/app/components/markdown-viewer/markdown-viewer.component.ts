85737409c13816326f05d8f4b588c23f0e0efc14
cvrain <cvraindays@outlook.com>
2025-08-11 17:05:16 +0800

    修复提示 Could not resolve "node_modules/marked/marked.min.js" [plugin angular-script-global]的错误

--- rainer-blog/src/app/components/markdown-viewer/markdown-viewer.component.ts
+++
@@ -1,8 +0,0 @@
-export class MarkdownViewer {
-  markdownText = '# hello world!'; // 初始化 Markdown 内容
-
-  // 添加一个方法来动态更新 Markdown 内容（如果需要）
-  updateMarkdown(newContent: string) {
-    this.markdownText = newContent;
-  }
-}
